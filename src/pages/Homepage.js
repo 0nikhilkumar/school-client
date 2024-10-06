b1057e37f53075a25aae29103c9e80ade7e601d5
Soul Predator <nnnnikhil5@gmail.com>
2024-10-06 23:08:26 +0530

    adding client

---
+++ src/pages/Homepage.js
@@ -0,0 +1,85 @@
+import { Box, Container, Grid } from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Students from "../assets/subjects.svg";
+import { LightPurpleButton } from '../components/buttonStyles';
+
+const Homepage = () => {
+    return (
+        <StyledContainer>
+            <Grid container spacing={0}>
+                <Grid item xs={12} md={6}>
+                    <img src={Students} alt="students" style={{ width: '100%' }} />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <StyledPaper elevation={3}>
+                        <StyledTitle>
+                            Welcome to
+                            <br />
+                            School Management
+                            <br />
+                            Site
+                        </StyledTitle>
+                        <StyledText>
+                        A School Management System is a comprehensive software solution designed to streamline the administrative, academic, and operational processes of educational institutions. It manages key functions like student enrollment, attendance, grade tracking, timetable scheduling, staff management, and communication between teachers, students, and parents.
+                        </StyledText>
+                        <StyledBox>
+                            <StyledLink to="/choose">
+                                <LightPurpleButton variant="contained" fullWidth>
+                                    Login
+                                </LightPurpleButton>
+                            </StyledLink>
+                        </StyledBox>
+                    </StyledPaper>
+                </Grid>
+            </Grid>
+        </StyledContainer>
+    );
+};
+
+export default Homepage;
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const StyledPaper = styled.div`
+  padding: 24px;
+  height: 100vh;
+`;
+
+const StyledBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content:center;
+  gap: 16px;
+  padding: 24px;
+  backgroundColor: "#000000"
+`;
+
+const StyledTitle = styled.h1`
+  font-size: 3rem;
+  color: #252525;
+  /* font-family: "Manrope"; */
+  font-weight: bold;
+  padding-top: 0;
+  letter-spacing: normal;
+  line-height: normal;
+`;
+
+const StyledText = styled.p`
+  /* color: #550080; */
+  margin-top: 30px;
+  margin-bottom: 30px; 
+  letter-spacing: normal;
+  line-height: normal;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
